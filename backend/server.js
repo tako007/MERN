c32b7f50d1be380892d4362993ee4f0f38ee174d
Tayfun Yiğit <tayfunyigit02@gmail.com>
2024-03-03 12:01:07 +0300

    router oluşturuldu

--- backend/server.js
+++ backend/server.js
@@ -2,11 +2,14 @@
 require("dotenv").config()
 
 const express = require("express")
+const workoutRoutes = require("./routes/workouts")
 
 // express app
 const app = express()
 
 // middleware
+app.use(express.json())
+
 app.use( (req,res,next) => {
     console.log(req.path, req.method);
     next();
@@ -14,9 +17,7 @@ app.use( (req,res,next) => {
 
 
 // routes
-app.get("/", (req, res) => {
-    res.json({mssg: "Welcome to the app"})
-})
+app.use("/api/workouts", workoutRoutes)
 
 // listen for requests
 app.listen(process.env.PORT, () => {
