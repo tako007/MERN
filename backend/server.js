1c0ee70ff9e5c6a7af045056ce457b3b8c033be5
Tayfun Yiğit <tayfunyigit02@gmail.com>
2024-03-03 13:08:40 +0300

    connect to mongodb

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@
 require("dotenv").config()
 
 const express = require("express")
+const mongoose = require("mongoose")
 const workoutRoutes = require("./routes/workouts")
 
 // express app
@@ -10,7 +11,7 @@ const app = express()
 // middleware
 app.use(express.json())
 
-app.use( (req,res,next) => {
+app.use((req, res, next) => {
     console.log(req.path, req.method);
     next();
 })
@@ -19,8 +20,17 @@ app.use( (req,res,next) => {
 // routes
 app.use("/api/workouts", workoutRoutes)
 
-// listen for requests
-app.listen(process.env.PORT, () => {
-    console.log("Listening on port 4000");
-})
+// connect to db
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        // listen for requests
+        app.listen(process.env.PORT, () => {
+            console.log("connected to db & listening on port", process.env.PORT);
+        })
+    })
+    .catch((error) = > {
+        console.log(error);
+    })
+
+
 
